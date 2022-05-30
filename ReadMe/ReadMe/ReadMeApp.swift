6d37f748aee663de2511c5337078f38bbc230712
sagar kadam <misc.sagarkadam@gmail.com>
2022-05-30 09:31:18 +0530

    Code Update : Initial Commit

---
+++ ReadMe/ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Sagar Kadam on 30/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
