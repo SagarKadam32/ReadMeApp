a5e83278798d34717e8b450b5d5ded8575e7a917
sagar kadam <misc.sagarkadam@gmail.com>
2022-06-02 10:32:21 +0530

    Code Update : Added the navigation view and the navigation link to the Text Image Views.

--- ReadMe/ReadMe/Views/ContentView.swift
+++ ReadMe/ReadMe/Views/ContentView.swift
@@ -10,27 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
   
-        List(Library().sortedBooks, id: \.title) { book in
-            BookRowView(book: book)
+        NavigationView {
+            List(Library().sortedBooks, id: \.title) { book in
+                BookRowView(book: book)
+            }
+            .navigationTitle("My Library")
         }
-
     }
 }
 
 struct BookRowView : View {
     let book : Book
     var body: some View {
-        HStack {
-            Book.Image(title: book.title)
-            VStack(alignment: .leading) {
-                Text(book.title)
-                    .font(.title2)
-                Text(book.author)
-                    .font(.title3)
-                    .foregroundColor(.secondary)
-            }.lineLimit(1)
+        NavigationLink(destination: DetailView(book: book)) {
+            HStack {
+                Book.Image(title: book.title)
+                VStack(alignment: .leading) {
+                    Text(book.title)
+                        .font(.title2)
+                    Text(book.author)
+                        .font(.title3)
+                        .foregroundColor(.secondary)
+                }.lineLimit(1)
+            }
+            .padding(.vertical)
         }
-        .padding(.vertical)
+
     }
 }
 
